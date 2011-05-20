27e2da16883892f25a8a7cdba3e12881a22013f8
Dmitry Guyvoronsky <dmitry.guyvoronsky@gmail.com>
2011-05-20 23:27:04 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='dpvt',
+      version=version,
+      description="Django project templates that support deployment to vagrant-managed virtual box",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='django fabric vagrant',
+      author='Dmitry Guyvoronsky',
+      author_email='dmitry.guyvoronsky@gmail.com',
+      url='http://demiurg.com.ua/dpvt',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+        'PasteScript>=1.3',
+        'Cheetah',
+      ],
+      entry_points="""
+      [paste.paster_create_template]
+      """,
+      )
