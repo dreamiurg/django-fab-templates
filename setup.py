c896dbd3b4f61a3bbee2ce46a3b684756d117157
Dmitry Guyvoronsky <dmitry.guyvoronsky@gmail.com>
2011-05-21 01:35:26 +0300

    Fixed packaging issues

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ import sys, os
 
 version = '0.1'
 
-setup(name='django-fab-templates',
-      version=version,
-      description="Django project templates that support fabric-style deployment (vagrant+ubuntu, webfaction)",
-      classifiers=[
+setup(name = 'django-fab-templates',
+      version = version,
+      description = "Django project templates that support fabric-style deployment (vagrant+ubuntu, webfaction)",
+      classifiers = [
         "Programming Language :: Python",
         "Development Status :: 3 - Alpha",
         "Environment :: Console",
@@ -16,22 +16,24 @@ setup(name='django-fab-templates',
         "Operating System :: OS Independent",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Software Development :: Code Generators",
-        ], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords=['django', 'fabric', 'vagrant', 'template', 'project', 'html5', 'boilerplate', 'vm', 'virtualbox'],
-      author='Dmitry Guyvoronsky',
-      author_email='dmitry.guyvoronsky@gmail.com',
-      url='',
-      license='MIT',
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
+        ], # Get strings from http://pypi.python.org/pypi?%3Aaction = list_classifiers
+      keywords = ['django', 'fabric', 'vagrant', 'template',
+                  'project', 'html5', 'boilerplate', 'vm', 'virtualbox', 'paster'],
+      author = 'Dmitry Guyvoronsky',
+      author_email = 'dmitry.guyvoronsky@gmail.com',
+      url = '',
+      license = 'MIT',
+      packages = find_packages('src'),
+      package_dir = {'':'src'},
+      include_package_data = True,
+      zip_safe = False,
+      install_requires = [
         'setuptools',
         'PasteScript>=1.3',
         'Cheetah',
       ],
-      entry_points="""
+      entry_points = """
       [paste.paster_create_template]
-      vagrant_project=src.pastertemplates:VagrantTemplate
+      vagrant_project = django_fab_templates.pastertemplates:VagrantTemplate
       """,
       )
